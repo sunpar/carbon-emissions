7528e08e2af94495257eb33ecd7b4ffb533b265f
Sundeep Paruchuri <sundeep@Sundeeps-MBP.home>
2019-05-11 13:59:24 -0400

    beeswarm

--- app/Qlik/Object-Props/flightCO2.js
+++ app/Qlik/Object-Props/flightCO2.js
@@ -1,45 +1,156 @@
 const itinHistogramProps = {
-    qInfo: {
-      qType: 'itinHistogram'
-    },
-    qHyperCubeDef: {
-      qInitialDataFetch: [
-        {
-          qHeight: 0,
-          qWidth: 0
-        }
-      ],
-      qDimensions: [
-        {
-          qDef: {
-            qFieldDefs: ['[%UN Travel Row ID]'],
-            qSortCriterias: [
-              {
-                qSortByNumeric: 1,
-                qSortByAscii: 0,
-                qSortByLoadOrder: 0,
-                qExpression: {}
-              }
-            ]
-          },
-          qNullSuppression: true
-        }
-      ],
-      qMeasures: [
-        {
-          qDef: {
-            qDef: '=Only([CO2 After Offsetting (kg)])',
-            qLabel: 'Carbon'
-          },
-          qLibraryId: null
-        }
-      ],
-      qInterColumnSortOrder: [0, 1],
-      qSuppressZero: false,
-      qSuppressMissing: false,
-      qMode: 'S'
-    }
-  };
-  
-  export default itinHistogramProps;
-  
+  qInfo: {
+    qType: 'itinHistogram'
+  },
+  qHyperCubeDef: {
+    qInitialDataFetch: [
+      {
+        qHeight: 0,
+        qWidth: 0
+      }
+    ],
+    qDimensions: [
+      {
+        qDef: {
+          qFieldDefs: ['=1'],
+          qSortCriterias: [
+            {
+              qSortByNumeric: 1,
+              qSortByAscii: 0,
+              qSortByLoadOrder: 0,
+              qExpression: {}
+            }
+          ]
+        },
+        qNullSuppression: true
+      },
+      {
+        qDef: {
+          qFieldDefs: ['[%UN Travel Row ID]'],
+          qSortCriterias: [
+            {
+              qSortByNumeric: 1,
+              qSortByAscii: 0,
+              qSortByLoadOrder: 0,
+              qExpression: {}
+            }
+          ]
+        },
+        qNullSuppression: true
+      },
+      {
+        qDef: {
+          qFieldDefs: ['Itinerary'],
+          qSortCriterias: [
+            {
+              qSortByNumeric: 1,
+              qSortByAscii: 0,
+              qSortByLoadOrder: 0,
+              qExpression: {}
+            }
+          ]
+        },
+        qNullSuppression: true
+      },
+      {
+        qDef: {
+          qFieldDefs: ['[Departure Date]'],
+          qSortCriterias: [
+            {
+              qSortByNumeric: 1,
+              qSortByAscii: 0,
+              qSortByLoadOrder: 0,
+              qExpression: {}
+            }
+          ]
+        },
+        qNullSuppression: true
+      },
+      {
+        qDef: {
+          qFieldDefs: ['[Class]'],
+          qSortCriterias: [
+            {
+              qSortByNumeric: 1,
+              qSortByAscii: 0,
+              qSortByLoadOrder: 0,
+              qExpression: {}
+            }
+          ]
+        },
+        qNullSuppression: true
+      },
+      {
+        qDef: {
+          qFieldDefs: ['[Travel Category]'],
+          qSortCriterias: [
+            {
+              qSortByNumeric: 1,
+              qSortByAscii: 0,
+              qSortByLoadOrder: 0,
+              qExpression: {}
+            }
+          ]
+        },
+        qNullSuppression: true
+      },
+      {
+        qDef: {
+          qFieldDefs: ['[Travel Type]'],
+          qSortCriterias: [
+            {
+              qSortByNumeric: 1,
+              qSortByAscii: 0,
+              qSortByLoadOrder: 0,
+              qExpression: {}
+            }
+          ]
+        },
+        qNullSuppression: true
+      },
+      {
+        qDef: {
+          qFieldDefs: ['[Traveler Type]'],
+          qSortCriterias: [
+            {
+              qSortByNumeric: 1,
+              qSortByAscii: 0,
+              qSortByLoadOrder: 0,
+              qExpression: {}
+            }
+          ]
+        },
+        qNullSuppression: true
+      }
+    ],
+    qMeasures: [
+      {
+        qDef: {
+          qDef: '=Only([CO2 After Offsetting (kg)])',
+          qLabel: 'Carbon'
+        },
+        qLibraryId: null
+      },
+      {
+        qDef: {
+          qDef: '=Only([Trip Distance (km)])',
+          qLabel: 'Carbon'
+        },
+        qLibraryId: null
+      },
+      {
+        qDef: {
+          qDef: '=Count(Distinct [%Legs_RowID])',
+          qLabel: 'Carbon'
+        },
+        qLibraryId: null
+      }
+    ],
+    qInterColumnSortOrder: [0, 1, 2],
+    qSuppressZero: false,
+    qSuppressMissing: false,
+    qMode: 'S'
+  }
+};
+
+export default itinHistogramProps;
--- app/components/Analysis/beeswarm-tile/beeswarm-tile.js
+++ app/components/Analysis/beeswarm-tile/beeswarm-tile.js
@@ -16,16 +16,32 @@ const BeeSwarmTile = () => {
   const { app$ } = useContext(qlikContext);
   const { handle, data } = useObjectData(flightCO2, app$);
   const [chartNum, setChartNum] = useState(0);
+  const [valueNum, setValueNum] = useState(0);
+  const [colorNum, setColorNum] = useState(1);
 
   if (data) {
-    console.log(handle, data);
+    const formatData = data
+      .map(row => ({
+        tooltips: [
+          { label: 'Itinerary', value: row[2].qText },
+          { label: 'Departure Date', value: row[3].qText },
+          { label: 'Travel Class', value: row[4].qText },
+          { label: 'Travel Category', value: row[5].qText },
+          { label: 'Travel Type', value: row[6].qText },
+          { label: 'Traveler Type', value: row[7].qText },
+          { label: '# Flights', value: row[10].qNum }
+        ],
+        value: [row[8].qNum, row[9].qNum][valueNum],
+        colorValue: [row[8].qNum, row[9].qNum, row[10].qNum][colorNum]
+      }))
+      .slice(0, 1000);
 
     const handleChange = (event, newValue) => {
       setChartNum(newValue);
     };
 
     return (
-      <TileComponent title="Trends">
+      <TileComponent title="Distribution">
         <div className={styles.trendsContainer}>
           <div className={styles.tabContainer}>
             <Tabs value={chartNum} onChange={handleChange}>
@@ -36,7 +52,7 @@ const BeeSwarmTile = () => {
             </Tabs>
           </div>
           <div className={styles.chartContainer}>
-            <Beeswarm data={data} />
+            <Beeswarm data={formatData} />
           </div>
         </div>
       </TileComponent>
